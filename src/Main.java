9c218a39d5feec9c66f8dfebe5c20f3d1638660d
TheRiseOfDavid <david53133@gmail.com>
2025-04-09 18:04:41 +0800

    --amend

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,10 @@ public class Main {
     static final int UIHeight = 600;
 
     public static void main(String[] args) throws Exception {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
         JFrame frame = new JFrame("UML Editor");
+        frame.setSize((int) (screenSize.width * 0.8), (int) (screenSize.height * 0.8));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(UIWidth, UIHeight);
 
